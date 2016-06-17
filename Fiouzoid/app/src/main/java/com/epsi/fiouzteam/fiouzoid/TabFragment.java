485554b53c61d87da3703e2a8ba591075c386660
lezhumain <julien.merion@epsi.fr>
2016-06-17 19:08:30 +0200

    fixes

--- Fiouzoid/app/src/main/java/com/epsi/fiouzteam/fiouzoid/TabFragment.java
+++ Fiouzoid/app/src/main/java/com/epsi/fiouzteam/fiouzoid/TabFragment.java
@@ -33,11 +33,11 @@ public class TabFragment extends Fragment {
         super();
 
         // TODO: parameters and load displayed group infos
-        Bundle args = getArguments()
-        String groupName = args.getString("groupName");
+//        Bundle args = getArguments();
+//        String groupName = args.getString("groupName");
 
-        mGroup = Database.mGroupDao.fetchByName(groupName);
-        Log.i(TAG, mGroup.toString());
+//        mGroup = Database.mGroupDao.fetchByName(groupName);
+//        Log.i(TAG, mGroup.toString());
     }
 
     @Nullable
